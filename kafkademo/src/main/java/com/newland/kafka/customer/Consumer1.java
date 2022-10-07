603be7e855e0c1cc43de9f64ff7dca8a3f114479
leellun <leellun@sina.cn>
2022-10-07 23:22:01 +0800

    同步发送 异步回调发送 自动确认 手动确认 事务消息 分区查看 acks设置

--- kafkademo/src/main/java/com/newland/kafka/customer/Consumer1.java
+++ kafkademo/src/main/java/com/newland/kafka/customer/Consumer1.java
@@ -29,10 +29,10 @@ public class Consumer1 {
         properties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
 
         // 配置消费者组id
-        properties.put(ConsumerConfig.GROUP_ID_CONFIG, "test0");
+        properties.put(ConsumerConfig.GROUP_ID_CONFIG, "test00");
 
         // 设置分区分配策略
-        properties.put(ConsumerConfig.PARTITION_ASSIGNMENT_STRATEGY_CONFIG, "org.apache.kafka.clients.consumer.StickyAssignor");
+        properties.put(ConsumerConfig.PARTITION_ASSIGNMENT_STRATEGY_CONFIG, "org.apache.kafka.clients.consumer.RoundRobinAssignor");
 
         KafkaConsumer<String, String> kafkaConsumer = new KafkaConsumer<String, String>(properties);
 
